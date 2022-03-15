ea0dadb023fc24fbd7849b24a80db350397f9bf1
D1 <D1@corp.techouts.local>
2022-03-15 15:19:27 +0530

    PrintingFloat

---
+++ techouts/PrintingFloat.java
@@ -0,0 +1,13 @@
+package org.techouts;
+
+public class PrintingFloat {
+
+	public static void main(String[] args) {
+		int Number = 10;
+		float Number2 = Number;
+		System.out.println(Number);
+		System.out.println(Number2);
+
+	}
+
+}
