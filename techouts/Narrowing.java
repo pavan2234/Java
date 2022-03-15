e39f7eeda0e92d2bd44c8e4f580d2c29ba4ab64e
D1 <D1@corp.techouts.local>
2022-03-15 16:02:11 +0530

    PrintingFloat

---
+++ techouts/Narrowing.java
@@ -0,0 +1,13 @@
+package org.techouts;
+
+public class Narrowing {
+
+	public static void main(String[] args) {
+		float number = 10;
+		int number2 = (int)number;
+		System.out.println(number);
+		System.out.println(number2);
+
+	}
+
+}
---
+++ techouts/Widening.java
@@ -0,0 +1,13 @@
+package org.techouts;
+
+public class Widening {
+
+	public static void main(String[] args) {
+		int number = 10;
+		float number2 = number;
+		System.out.println(number);
+		System.out.println(number2);
+
+	}
+
+}
