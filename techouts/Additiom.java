d3b36bc7b566dd72c5db394553f484ad5cbedd7d
D1 <D1@corp.techouts.local>
2022-03-15 13:44:34 +0530

    AddingTwoNumbers

---
+++ techouts/Additiom.java
@@ -0,0 +1,13 @@
+package org.techouts;
+
+public class Additiom {
+
+	public static void main(String[] args) {
+		int firstnumber = 10;
+		int secondnumber = 20;
+		int sum = firstnumber + secondnumber;
+		System.out.println(sum);
+
+	}
+
+}
